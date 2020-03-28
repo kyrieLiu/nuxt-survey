1abe7fe888b85fe77216bd4ba1665adf61b4bc84
15153246027 <652992429@qq.com>
2020-03-28 13:49:30 +0800

    xxxx

--- pages/second.vue
+++ pages/second.vue
@@ -5,26 +5,14 @@
  -->
 <template>
   <article class="statistic">
-    <van-tabs v-model="activeName" color="rgb(25,137,250)" sticky style="height: 100%;display: flex;flex-direction: column">
-      <van-tab title="调查数据" name="1" style="height: 100%">
-        <survey-list></survey-list>
-      </van-tab>
-      <van-tab title="图表统计" name="2" style="height: 100%">
-        <chart-statistic></chart-statistic>
-      </van-tab>
-    </van-tabs>
+    <chart-statistic></chart-statistic>
   </article>
 </template>
 
 <script>
-import { Tabs, Tab } from 'vant'
-import surveyList from '../components/surveyList'
 import chartStatistic from '../components/chartStatistic'
 export default {
   components: {
-    VanTab: Tab,
-    VanTabs: Tabs,
-    surveyList,
     chartStatistic
   },
   data() {
